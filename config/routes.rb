b7772b18e617d41aaa981043a0441a6dd9da7d0f
Roman Lugovtsev <omelemon@gmail.com>
2017-06-09 13:54:33 +0400

    Updated routes.

--- config/routes.rb
+++ config/routes.rb
@@ -4,5 +4,5 @@ Rails.application.routes.draw do
   get '/init', to: 'home#init', as: 'init'
   get '/callback', to: 'home#callback', as: 'callback'
   get '/calendars', to: 'home#calendars', as: 'calendars'
-  get '/events/:room_id', to: 'home#events', as: 'events'
+  get '/:room_id', to: 'home#events', as: 'events'
 end
